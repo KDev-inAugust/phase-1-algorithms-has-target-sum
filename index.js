3ea453de141de3ed28436735f85ee45f78d89291
Kane Mathis <admin@kanemathis.com>
2022-05-03 22:12:41 -0400

    commit tree 0d7aa235ce400e6747cb3a97f866c490520201c5

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let filteredArray=[];
+  let decidingArray=[];
+  for(x=0; x<array.length; x++){
+        if(array[x]<target){
+          filteredArray.push(array[x]);
+        }    
+  }
+
+  for(x=0; x<array.length; x++){
+    let workingRemainder;
+    let negativeCheck = Math.sign(array[x]);
+    if (negativeCheck===-1){
+      console.log("negative integer processed");
+      filteredArray.push(array[x]);
+      let negativeOffSetNumber = -array[x]+target;
+          array.find(element =>{
+            if (element===negativeOffSetNumber){
+              decidingArray.push(element, array[x]);
+              }
+          })
+    
+    }
+
+      if (negativeCheck===1)
+      {console.log('positive integer processed');
+      workingRemainder = target-filteredArray[x];
+        filteredArray.find(element=> {if (element===workingRemainder){
+                      decidingArray.push(element)
+                      
+              }
+          }
+        )
+      }
+  }
+  console.log("filtered Array " + filteredArray);
+  console.log("deciding array  "+ decidingArray + "  target " + target)
+  
+  if (decidingArray.length>=2){
+    return true
+    }
+    return false;
 }
 
 /* 
@@ -8,10 +49,24 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  For each number less than the target see if the remainder is in the array  
+  As a point of efficiecy we can rule out 
+  any number larger that the target
+  and any number that is one less than the target if there is no number 1
 */
 
 /*
   Add written explanation of your solution here
+  first make a filtered array of all numbers less than the target number 
+  as they are all candidates becasue they could be added to to make the target.
+  then if an integer is positive see if there is another positive integer that equals the 
+  remainder of the target minus that first integer. if there is add them to the deciding array.
+
+  for negative integers make that number positive (negating it) then add the 
+  target to it and see if there is number in the array that equals that. if so add them both to the deciding 
+  array as they will create the target. 
+
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -27,8 +82,14 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log('Expecting True')
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
+ console.log("=>", hasTargetSum([2,7,8,4,22,-5],17));
